ff082a91b649fc35573a1b8c88934a7ffac1521d
solpark16 <79960346+solpark16@users.noreply.github.com>
2024-04-18 12:38:34 +0900

    밸런스 게임 수정

--- js/script.js
+++ js/script.js
@@ -20,31 +20,31 @@ $().ready(function () {
 });
 
 // Balance Game
-function mouseenterHandler1() {document.getElementById("naeng").textContent="3 물냉 vs 비냉 3";}
+function mouseenterHandler1() {document.getElementById("naeng").textContent="4 vs 2";}
 function mouseleaveHandler1() {document.getElementById("naeng").textContent="물냉 vs 비냉";}
 
-function mouseenterHandler2() {document.getElementById("tang").textContent="2 부먹 vs 찍먹 4";}
+function mouseenterHandler2() {document.getElementById("tang").textContent="1 vs 5";}
 function mouseleaveHandler2() {document.getElementById("tang").textContent="부먹 vs 찍먹";}
 
-function mouseenterHandler3() {document.getElementById("mint").textContent="3 민초 vs 반민초 3";}
+function mouseenterHandler3() {document.getElementById("mint").textContent="5 vs 1";}
 function mouseleaveHandler3() {document.getElementById("mint").textContent="민초 vs 반민초";}
 
-function mouseenterHandler4() {document.getElementById("drink").textContent="4 코카콜라 vs 펩시 2";}
-function mouseleaveHandler4() {document.getElementById("drink").textContent="코카콜라 vs 펩시";}
+function mouseenterHandler4() {document.getElementById("drink").textContent="2 vs 4";}
+function mouseleaveHandler4() {document.getElementById("drink").textContent="코카콜라 vs 펩시콜라";}
 
-function mouseenterHandler5() {document.getElementById("peach").textContent="4 물복 vs 딱복 2";}
+function mouseenterHandler5() {document.getElementById("peach").textContent="5 vs 1";}
 function mouseleaveHandler5() {document.getElementById("peach").textContent="물복 vs 딱복";}
 
-function mouseenterHandler6() {document.getElementById("fishbun").textContent="4 팥붕 vs 슈붕 2";}
+function mouseenterHandler6() {document.getElementById("fishbun").textContent="5 vs 1";}
 function mouseleaveHandler6() {document.getElementById("fishbun").textContent="팥붕 vs 슈붕";}
 
-function mouseenterHandler7() {document.getElementById("noodle").textContent="2 짜장 vs 짬뽕 4";}
+function mouseenterHandler7() {document.getElementById("noodle").textContent="3 vs 3";}
 function mouseleaveHandler7() {document.getElementById("noodle").textContent="짜장 vs 짬뽕";}
 
-function mouseenterHandler8() {document.getElementById("coffee").textContent="1 뜨아 vs 아아 5";}
+function mouseenterHandler8() {document.getElementById("coffee").textContent="1 vs 5";}
 function mouseleaveHandler8() {document.getElementById("coffee").textContent="뜨아 vs 아아";}
 
-function mouseenterHandler9() {document.getElementById("pig").textContent="3 삼겹살 vs 목살 3";}
+function mouseenterHandler9() {document.getElementById("pig").textContent="4 vs 2";}
 function mouseleaveHandler9() {document.getElementById("pig").textContent="삼겹살 vs 목살";} 
 
 // scroll animation
