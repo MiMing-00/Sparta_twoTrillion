eb3938d2f0f173de98e17ef062e3924f2b49cd89
oneieo <sun020716@gmail.com>
2024-04-19 17:27:05 +0900

    키 숨기기

--- js/privateSjw.js
+++ js/privateSjw.js
@@ -1,10 +1,12 @@
 <script>
+
 const firebaseConfig = {
   apiKey: "AIzaSyDtjTmympAAkcLbRTFVKAFrCKc1CwRo1D0",
   authDomain: "member-comment.firebaseapp.com",
   projectId: "member-comment",
   storageBucket: "member-comment.appspot.com",
   messagingSenderId: "174671122442",
-  appId: "1:174671122442:web:6d72c7af4a0fda299ecff3"
+  appId: "1:174671122442:web:6d72c7af4a0fda299ecff3",
 };
+
 </script>
