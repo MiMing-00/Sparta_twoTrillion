c34400761af7c5cd1378ba19d64fb8ab54463204
oneieo <sun020716@gmail.com>
2024-04-19 17:33:09 +0900

    3억...

--- js/privateSjw.js
+++
@@ -1,9 +0,0 @@
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDtjTmympAAkcLbRTFVKAFrCKc1CwRo1D0",
-  authDomain: "member-comment.firebaseapp.com",
-  projectId: "member-comment",
-  storageBucket: "member-comment.appspot.com",
-  messagingSenderId: "174671122442",
-  appId: "1:174671122442:web:6d72c7af4a0fda299ecff3",
-};
